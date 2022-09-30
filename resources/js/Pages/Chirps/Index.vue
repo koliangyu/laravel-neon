2bcd68651ebf44032ee0f879b66d74ef7787b778
Kuro Usagi <klw19960615@gmail.com>
2022-09-30 17:39:13 +0800

    Showing Chirps

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { useForm, Head } from '@inertiajs/inertia-vue3';
 import AppLayout from '@/Layouts/AppLayout.vue';
+import Chirp from '@/Components/Chirp.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+
+defineProps(['chirps']);
 
 const form = useForm({
     message: '',
@@ -25,6 +28,10 @@ const form = useForm({
                     <InputError :message="form.errors.message" class="mt-2" />
                     <PrimaryButton class="mt-4">Chirp</PrimaryButton>
                 </form>
+
+                <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                    <Chirp v-for="chirp in chirps" :key="chirp.id" :chirp="chirp" />
+                </div>
             </div>
         </div>
     </AppLayout>
