b764fa04a8f8488827ab8b7cf98d6a3f7d13ad12
Kuro Usagi <klw19960615@gmail.com>
2022-09-30 17:31:47 +0800

    Creating Chirps

---
+++ resources/js/Pages/Chirps/Index.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useForm, Head } from '@inertiajs/inertia-vue3';
+import AppLayout from '@/Layouts/AppLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+
+const form = useForm({
+    message: '',
+});
+</script>
+
+<template>
+    <AppLayout title="Chirps">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Chirps
+            </h2>
+        </template>
+
+        <div>
+            <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+                <form @submit.prevent="form.post(route('chirps.store'), { onSuccess: () => form.reset() })">
+                    <textarea v-model="form.message" placeholder="What's on your mind?"
+                        class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
+                    <InputError :message="form.errors.message" class="mt-2" />
+                    <PrimaryButton class="mt-4">Chirp</PrimaryButton>
+                </form>
+            </div>
+        </div>
+    </AppLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -32,4 +33,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+
+    Route::resource('chirps', ChirpController::class)
+        ->only(['index', 'store']);
 });
